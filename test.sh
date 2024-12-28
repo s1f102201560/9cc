db6bd2d97fa8ed2826f309c36af831cfe044f7ad
1F10220156 <s1f102201560@iniad.org>
2024-12-28 16:33:01 +0900

    :fire:

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s
+  cc ./tmp.s -o tmp
+  ./tmp
+
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 42 42
+assert 0 0
+
+echo OK
