4f321a9fd339c6d3cec93555e000a287aa35f27b
1F10220156 <s1f102201560@iniad.org>
2024-12-30 23:23:32 +0900

    :fire:

--- 9cc.c
+++ 9cc.c
@@ -107,25 +107,23 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  user_input = argv[1]; // " 12 + 34 - 5 "
-  token = tokenize();
+  user_input = argv[1];
+  token = tokenize(user_input);
 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
   printf("  mov rax, %d\n", expect_number());
-  
   while(!at_eof()) {
     if (consume('+')) {
       printf("  add rax, %d\n", expect_number());
       continue;
     }
-
     expect('-');
     printf("  sub rax, %d\n", expect_number());
   }
-
   printf("  ret\n");
+
   return 0;
 }
