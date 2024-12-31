04ae05dedfb046fecb557add256e00a0d516d9f2
1F10220156 <s1f102201560@iniad.org>
2024-12-31 16:53:45 +0900

    :fire:

--- 9cc.c
+++ 9cc.c
@@ -5,129 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum {
-  TK_NUM,
-  TK_RESERVED,
-  TK_EOF
-} TokenKind;
+// error_at
 
-typedef struct Token Token;
+// consume
 
-struct Token{
-  Token *next;
-  TokenKind kind;
-  char *str;
-  int val;
-};
+// expect
 
-Token *token;
-char *user_input;
+// expect_number
 
-void error_at(char *loc, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  
-  int pos = loc - user_input;
+// at_eof
 
-  fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, " ");
-  fprintf(stderr, "^ ");
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
+// new_token
 
-  exit(1);
-}
+// tokenize
 
-bool consume(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    return false;
-  token = token->next;
-  return true;
-}
-
-void expect(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    error_at(token->str, "'%c' expected", op);
-  token = token->next;
-}
-
-int expect_number() {
-  if (token->kind != TK_NUM)
-    error_at(token->str, "数値ではありません");
-  
-  int val = token->val;
-  token = token->next;
-  return val;
-}
-
-bool at_eof() {
-  return token->kind == TK_EOF;
-}
-
-Token *new_token(TokenKind kind, Token *cur, char *str) {
-  Token *tok = calloc(1, sizeof(Token));
-  tok->kind = kind;
-  tok->str = str;
-  cur->next = tok;
-  return tok;
-}
-
-Token *tokenize(char *p) {
-  Token head;
-  head.next = NULL;
-  Token *cur = &head;
-
-  while(*p) {
-    if (isspace(*p)) {
-      p++;
-      continue;
-    }
-
-    if (*p == '+' || *p == '-') {
-      cur = new_token(TK_RESERVED, cur, p);
-      p++;
-      continue;
-    }
-
-    if (isdigit(*p)) {
-      cur = new_token(TK_NUM, cur, p);
-      cur->val = strtol(p, &p, 10);
-      continue;
-    }
-
-    error_at(p, "トークナイズできません");
-  }
-  cur = new_token(TK_EOF, cur, p);
-
-  return head.next;
-}
-
-int main(int argc, char **argv)
-{
-  if (argc != 2) {
-    fprintf(stderr, "引数の個数が誤っています\n");
-    return 1;
-  }
-
-  user_input = argv[1];
-  token = tokenize(user_input);
-
-  printf(".intel_syntax noprefix\n");
-  printf(".globl main\n");
-  printf("main:\n");
-  
-  printf("  mov rax, %d\n", expect_number());
-
-  while(!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-  printf("  ret\n");
-
-  return 0;
-}
+// main
