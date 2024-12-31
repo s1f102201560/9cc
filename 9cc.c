a8999eaffb9e9dcf14b017c3e1a226d488c7c9ca
1F10220156 <s1f102201560@iniad.org>
2024-12-31 16:10:33 +0900

    :fire:

--- 9cc.c
+++ 9cc.c
@@ -24,18 +24,7 @@ Token *token;
 
 char *user_input;
 
-void error_at(char *loc, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-
-  int pos = loc - user_input;
-  fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, " ");
-  fprintf(stderr, "^ ");
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
+// error_at
 
 bool consume(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
@@ -50,82 +39,17 @@ void expect(char op) {
   token = token->next;
 }
 
-int expect_number() {
-  if (token->kind != TK_NUM)
-    error_at(token->str, "expected a number");
-  int val = token->val;
-  token = token->next;
-  return val;
-}
+// expect_number
 
 bool at_eof() {
   return token->kind == TK_EOF;
 }
 
-Token *new_token(TokenKind kind, Token *cur, char *str) {
-  Token *tok = calloc(1, sizeof(Token));
-  tok->kind = kind;
-  tok->str = str;
-  cur->next = tok;
-  return tok;
-}
+// new_token
 
 
-Token *tokenize(char *p) {
-  Token head;
-  head.next = NULL;
-  Token *cur = &head;
-
-  while(*p) {
-    if (isspace(*p)) {
-      p++;
-      continue;
-    }
-
-    if (*p == '+' || *p == '-') {
-      cur = new_token(TK_RESERVED, cur, p);
-      p++;
-      continue;
-    }
-
-    if (isdigit(*p)) {
-      cur = new_token(TK_NUM, cur, p);
-      cur->val = strtol(p, &p, 10);
-      continue;
-    }
-
-    error_at(p, "トークナイズできません");
-  }
-  cur = new_token(TK_EOF, cur, p);
-
-  return head.next;
-}
+// tokenize
 
 int main(int argc, char **argv)
 {
-  if (argc != 2) {
-    fprintf(stderr, "引数の個数が誤っています\n");
-    return 1;
-  }
-
-  user_input = argv[1];
-  token = tokenize(argv[1]);
-
-  printf(".intel_syntax noprefix\n");
-  printf(".globl main\n");
-  printf("main:\n");
-  printf("  mov rax, %d\n", expect_number());
-
-  while(!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-  printf("  ret\n");
-
-  return 0;
 }
