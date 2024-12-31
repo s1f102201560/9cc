a276bf3997ea960aef434ee8f1bd1f8115a8b6c2
1F10220156 <s1f102201560@iniad.org>
2024-12-31 13:14:03 +0900

    :fire:

--- 9cc.c
+++ 9cc.c
@@ -71,6 +71,35 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 }
 
 
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while(*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p);
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+    error_at(p, "トークナイズできません。");
+  }
+  cur = new_token(TK_EOF, cur, p);
+  return head.next;
+}
+
+
 
 int main(int argc, char **argv)
 {
