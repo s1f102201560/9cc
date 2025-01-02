e044e1d016ea51d3f75d1284871931396f693841
1F10220156 <s1f102201560@iniad.org>
2025-01-03 04:37:29 +0900

    :fire:

--- 9cc.c
+++ 9cc.c
@@ -150,18 +150,18 @@ struct Node {
 // new_node
 Node *new_node(NodeKind kind) {
   Node *node = calloc(1, sizeof(Node));
-
   node->kind = kind;
-
   return node;
 }
 
 // new_binary
 Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
   Node *node = new_node(kind);
+
   node->lhs = lhs;
   node->rhs = rhs;
-  return node;
+  
+  return node; 
 }
 
 // new_num
