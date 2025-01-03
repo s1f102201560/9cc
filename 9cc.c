348c997a382f556d9273d7983eab5d1a5f36202d
1F10220156 <s1f102201560@iniad.org>
2025-01-03 14:05:18 +0900

    :fire:

--- 9cc.c
+++ 9cc.c
@@ -212,7 +212,7 @@ Node *primary() {
   return new_num(expect_number());
 }
 
-
+// gen
 void gen(Node *node) {
   if (node->kind == ND_NUM) {
     printf("  push %d\n", node->val);
@@ -225,20 +225,24 @@ void gen(Node *node) {
   printf("  pop rdi\n");
   printf("  pop rax\n");
 
-  switch (node->kind) {
-  case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
-  case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
-  case ND_MUL:
-    printf("  imul rax, rdi\n");
-    break;
-  case ND_DIV:
-    printf("  cqo\n");
-    printf("  idiv rax, rdi\n");
-    break;
+  switch (node->kind)
+  {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rax, rdi\n");
+      break;
   }
 
   printf("  push rax\n");
