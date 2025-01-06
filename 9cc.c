18efac1cd2105d3e54869afa9b4e6a1cfceb3282
1F10220156 <s1f102201560@iniad.org>
2025-01-06 17:11:46 +0900

    :fire:

--- 9cc.c
+++ 9cc.c
@@ -126,119 +126,19 @@ Token *tokenize(char *p) {
 //  Parser
 //
 
-typedef enum {
-  ND_ADD,
-  ND_SUB,
-  ND_MUL,
-  ND_DIV,
-  ND_NUM
-} NodeKind;
-
-typedef struct Node Node;
-
-struct Node {
-  NodeKind kind;
-  Node *lhs;
-  Node *rhs;
-  int val;
-};
-
 // new_node
-Node *new_node(NodeKind kind) {
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = kind;
-  return node;
-}
 
 // new_binary
-Node *new_binary(NodeKind kind, Node* lhs, Node*rhs) {
-  Node *node = new_node(kind);
-  node->lhs = lhs;
-  node->rhs = rhs;
-  return node;
-}
 
 // new_num
-Node *new_num(int val) {
-  Node *node = new_node(ND_NUM);
-  node->val = val;
-  return node;
-}
-
-Node *expr();
-Node *mul();
-Node *primary();
 
 // expr
-Node *expr() {
-  Node *node = mul();
-
-  for (;;) {
-    if (consume('+'))
-      node = new_binary(ND_ADD, node, mul());
-    else if (consume('-'))
-      node = new_binary(ND_SUB, node, mul());
-    else
-      return node;
-  }
-}
 
 // mul
-Node *mul() {
-  Node *node = primary();
-
-  for (;;) {
-    if (consume('*'))
-      node = new_binary(ND_MUL, node, primary());
-    else if (consume('/'))
-      node = new_binary(ND_DIV, node, primary());
-    else
-      return node;
-  }
-}
 
 // primary
-Node *primary() {
-  if (consume('(')) {
-    Node *node = expr();
-    expect(')');
-    return node;
-  }
-  return new_num(expect_number());
-}
 
 // gen
-void gen(Node *node) {
-  if (node->kind == ND_NUM) {
-    printf("  push %d\n", node->val);
-    return;
-  }
-
-  gen(node->lhs);
-  gen(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
-
-  switch (node->kind)
-  {
-    case ND_ADD:
-      printf("  add rax, rdi\n");
-      break;
-    case ND_SUB:
-      printf("  sub rax, rdi\n");
-      break;
-    case ND_MUL:
-      printf("  imul rax, rdi\n");
-      break;
-    case ND_DIV:
-      printf("  cqo\n");
-      printf("  idiv rax, rdi\n");
-      break;
-  }
-
-  printf("  push rax\n");
-}
 
 
 // main
